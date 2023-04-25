a20e45c9a111c807a2e159e8f5168e8ecc017481
CarlosToribio7 <131767694+CarlosToribio7@users.noreply.github.com>
2023-04-25 13:33:59 +0200

    Update Proyecto Informatica.c

--- Proyecto Informatica.c
+++ Proyecto Informatica.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
 
 struct TLavapies {
-		char parametros;
-		float ph;
-		int conductividad;
-		int turbidez;
-		int coliformes;
-	};
-	
-int main () {
-	
-	FILE*fentrada, *fsalida;
-	struct TLavapies mes01;
-	char nombrefichero[100];
-	int numfichero=0;
-	
-	printf("Introduce el nombre del fichero que contine los datos \n");
-	scanf("%s", &nombrefichero);
-	
-	fentrada = fopen("nombrefichero", "r"); //read
-	if (fentrada == NULL) {
-		printf("Error en la apertura del fichero de entrada\n");
-		return 0;
-	}
-	fsalida = fopen("resultados.txt", "w"); // write
-	if (fsalida == NULL) {
-		printf("Error en la apertura del fichero de salida\n");
-		return 0;
-	}
-	   int valor=0;
-        while(fscanf(FILE, "%d", &valor) == 1) {
-            nombrefichero[numfichero++] = valor;
-        }
+    char parametros;
+    float ph;
+    int conductividad;
+    int turbidez;
+    int coliformes;
+};
 
-       
-	fclose(fentrada);
-	fclose(fsalida);
-	
+int main () {
+    FILE* fentrada, * fsalida;
+    struct TLavapies mes01;
+    char nombrefichero[100];
+    int numfichero = 0;
+
+    printf("Introduce el nombre del fichero que contiene los datos: ");
+    scanf("%s", nombrefichero);
+
+    fentrada = fopen(nombrefichero, "r"); //read
+    if (fentrada == NULL) {
+        printf("Error en la apertura del fichero de entrada\n");
+        return 0;
+    }
+    fsalida = fopen("resultados.txt", "w"); // write
+    if (fsalida == NULL) {
+        printf("Error en la apertura del fichero de salida\n");
+        return 0;
+    }
+    int valor = 0;
+    while (fscanf(fentrada, "%d", &valor) == 1) {
+        numfichero++;
+    }
+
+    printf("El archivo %s contiene %d valores\n", nombrefichero, numfichero);
+
+    fclose(fentrada);
+    fclose(fsalida);
+
+    return 0;
 }
